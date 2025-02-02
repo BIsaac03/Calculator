ba2432ababe5959bf4c0ec8f2c07ae73e558f17a
Benjamin Isaac <benisaac03@gmail.com>
2025-02-01 21:03:57 -0600

    Add button functionality along with new 'clear' button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let currentDisplay = document.querySelector("p.solution")
+let num1 = 0
+let num2 = 0
+let operation = 0
+let clearNext = false
+
 function add (a, b){
     return a+b
 }
@@ -16,28 +22,74 @@ function divide(a, b){
 
 function calculate(num1, num2, operation){
     if (operation == '+'){
-        return add(num1, num2)
+        return add(parseInt(num1), parseInt(num2))
     }
     else if (operation == '-'){
-        return subtract(num1, num2)
+        return subtract(parseInt(num1), parseInt(num2))
     }
     else if (operation == '*'){
-        return multiply(num1, num2)
+        return multiply(parseInt(num1), parseInt(num2))
     }
     else if (operation == '/'){
-        return divide(num1, num2)
+        return divide(parseInt(num1), parseInt(num2))
     }
     return undefined
 }
 
-let num1 = 0
-let num2 = 0
-let operation = 0
+function clearDisplay(){
+    currentDisplay.textContent = ''
+}
 
-console.log(calculate(1, 3, '+'))
-console.log(calculate(2, 3, '-'))
-console.log(calculate(6, 3, '/'))
-console.log(calculate(7, 3, '-'))
-console.log(calculate(1, 5, '*'))
-console.log(calculate(1, 2, '%'))
-console.log(calculate(5, 8, '/'))
+function addToDisplay(buttonID){
+    let buttonPressed = document.querySelector(buttonID)
+    currentDisplay.textContent += buttonPressed.textContent
+}
+
+const allBtns = document.querySelectorAll("button");
+allBtns.forEach((btn) => {
+    btn.addEventListener("mouseover", () => {
+        btn.style.backgroundColor = "gray"
+    });
+
+    btn.addEventListener("mouseout", () => {
+        btn.style.backgroundColor = "floralwhite"
+    });
+});
+
+const numBtns = document.querySelectorAll("button.number");
+numBtns.forEach((numBtn) => {
+    numBtn.addEventListener("click", () => {
+        // if the last button pressed was not a number, a new number will begin
+        if (clearNext){
+            clearDisplay()
+            clearNext = false
+        }
+        let id = "#" + numBtn.getAttribute("id")
+        addToDisplay(id)
+    });
+});
+
+const opBtns = document.querySelectorAll("button.operation");
+opBtns.forEach((opBtn) => {
+    opBtn.addEventListener("click", () => {
+        num1 = currentDisplay.textContent
+        num2 = 0
+        operation = opBtn.textContent
+        clearNext = true
+    });
+});
+
+const equalBtn = document.querySelector("#equals");
+equalBtn.addEventListener("click", () => {
+    num2 = currentDisplay.textContent
+    currentDisplay.textContent = calculate(num1, num2, operation)
+    clearNext = true
+});
+
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", () => {
+    clearDisplay()
+    num1 = 0
+    num2 = 0
+    operation = 0
+});
